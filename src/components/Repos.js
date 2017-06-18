e57d7feaf7aedfd71d084a48d7eafec78a474c8c
kade042 <dereje88@gmail.com>
2017-06-17 19:33:50 -0600

    fetching repos from github api

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+import 'whatwg-fetch';
 
 export default class Repos extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      repositories: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.github.com/users/pro-react/repos')
+    .then((response)=> response.json())
+    .then((responseData)=> {
+      this.setState({
+        repositories: responseData,
+      });
+    });
+  }
+
   render() {
+    let repos = this.state.repositories.map((repo)=> (
+      <li key={repo.id}>
+        {repo.name}
+      </li>
+    ));
     return (
-      <h1>REPOS</h1>
+      <div>
+        <h1>Github repos</h1>
+        <ul>
+          {repos}
+        </ul>
+      </div>
     );
   }
 }
