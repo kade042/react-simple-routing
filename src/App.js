68e6e5487322555d93b3a9f94fa2bc2247fa2f75
kade042 <dereje88@gmail.com>
2017-06-17 18:55:04 -0600

    routing using react-router

--- src/App.js
+++ src/App.js
@@ -1,49 +1,35 @@
 
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Router, Route, Link, browserHistory } from 'react-router';
+
 import About from './components/About';
 import Home from './components/Home';
 import Repos from './components/Repos';
 
 class App extends Component {
-  constructor() {
-    super(...arguments);
-    this.state = {
-      route: window.location.hash.substr(1),
-    };
-  }
-
-  componentDidMount() {
-    window.addEventListener('hashchange', () => {
-      this.setState({
-        route: window.location.hash.substr(1)
-      });
-    });
-
-  }
-
   render() {
-    var Child;
-    switch (this.state.route) {
-      case '/about': Child = About; break;
-      case '/repos': Child = Repos; break;
-      default: Child = Home;
-    }
-
     return (
       <div>
         <header>App</header>
         <menu>
           <ul>
-            <li><a href='#/about'>About</a></li>
-            <li><a href='#/repos'>Repos</a></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/repos'>Repos</Link></li>
           </ul>
         </menu>
-        <Child />
+        { this.props.children }
       </div>
     );
   }
 }
 
 
-render(<App />, document.getElementById('root'));
+render((
+  <Router history={ browserHistory }>
+    <Route path='/' component={ App } >
+      <Route path='about' component={About} />
+      <Route path='repos' component={Repos} />
+    </Route>
+  </Router>
+  ), document.getElementById('root'));
